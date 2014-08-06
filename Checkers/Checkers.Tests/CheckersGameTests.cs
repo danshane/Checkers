ce2af8c83d6034417009ac18cfcd0063746debe2
Daniel Shane <dan@danshane.co.uk>
2014-08-06 20:33:25 +0100

    Added initial test cases for CheckersGame and PiecePosition

---
+++ Checkers/Checkers.Tests/CheckersGameTests.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Tests
+{
+    [TestFixture]
+    class CheckersGameTests
+    {
+        [Test]
+        public void CanCreateNewGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Player1CanMakeInitialMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Player2CannotMakeInitialMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void CanMakeValidBasicMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void CannotMakeInvalidBasicMove()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Test]
+        public void CanMakeValidJumpMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void CannotMakeInvalidJumpMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Player1PieceBecomesKingedWhenBasicMovedToLastRow()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Player2PieceBecomesKingedWhenBasicMovedToLastRow()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Player1PieceBecomesKingedWhenJumpMovedToLastRow()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Player2PieceBecomesKingedWhenJumpMovedToLastRow()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void PlayerMustJumpIfJumpPossible()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void PlayerResumesGoIfJumpPossibleAfterJump()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Player1WinsIfNoPlayer2PiecesRemain()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Player2WinsIfNoPlayer1PiecesRemain()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void StalemateIfNoAvailableMoves()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Checkers/Checkers.Tests/PiecePositionTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Tests
+{
+    [TestFixture]
+    class PiecePositionTests
+    {
+        [Test]
+        public void CanCreateValidPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void CannotCreateInvalidPosition()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
